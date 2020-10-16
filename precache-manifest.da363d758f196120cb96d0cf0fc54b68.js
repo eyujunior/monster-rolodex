ce8c90c987bafe402d0e1fc3d61e34d8ac6b0b00
eyujunior <eyujunior052@gmail.com>
2020-10-16 11:37:38 -0700

    Updates

---
+++ precache-manifest.da363d758f196120cb96d0cf0fc54b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2edddec47eca2ec6775b78e32ccc10",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1ae795920b8ae948e276",
+    "url": "/monster-rolodex/static/css/main.086c2cad.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monster-rolodex/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae795920b8ae948e276",
+    "url": "/monster-rolodex/static/js/main.246c540a.chunk.js"
+  },
+  {
+    "revision": "b70a9c55c535ea67c1fc",
+    "url": "/monster-rolodex/static/js/runtime-main.80f93c1b.js"
+  }
+]);
